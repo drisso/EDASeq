9944fc10a1273110ea8d4b2dd894bad8695a1c88
Davide Risso <dar2062@med.cornell.edu>
2018-06-20 14:42:04 -0400

    Remove redundant methods

--- R/methods-SeqExpressionSet.R
+++ R/methods-SeqExpressionSet.R
@@ -451,7 +451,6 @@ setMethod(
           }
           )
 
-
 setMethod(
     f = "plotPCA",
     signature = signature(object="matrix"),
@@ -506,32 +505,6 @@ setMethod(
           }
           )
 
-setMethod(
-          f = "plotRLE",
-          signature = signature(x="matrix"),
-          definition = function(x,...) {
-              y <- log(x+1)
-              median <- apply(y, 1, median)
-              rle <- apply(y, 2, function(x) x - median)
 
-              boxplot(rle, ...)
-              abline(h=0, lty=2)
-          }
-          )
 
-setMethod(
-          f = "plotRLE",
-          signature = signature(x="SeqExpressionSet"),
-          definition = function(x, ...) {
-            if(ncol(counts(x))<=1) {
-              stop("At least two samples needed for the mean-difference plot.")
-            } else {
-              if(all(is.na(normCounts(x)))) {
-                counts <- counts(x)
-              } else {
-                counts <- normCounts(x)
-              }
-              plotRLE(counts, ...)
-            }
-          }
-          )
+
