992ff8b79249a074f425bc451f2b12978bc2ab6a
Davide Risso <risso.davide@gmail.com>
2020-02-27 12:43:19 +0000

    Remove getLengthAndGC test to avoid biomart in tests

--- tests/testthat/test_getLengthAndGC.R
+++
@@ -1,8 +0,0 @@
-context("Test GC-content and length.")
-
-test_that("getGeneLengthAndGCContent works", {
-  gcc <- getGeneLengthAndGCContent(id=c("ENSG00000012048", "ENSG00000139618"), org="hsa", mode = "biomart")
-
-  expect_equal(nrow(gcc), 2)
-}
-)
